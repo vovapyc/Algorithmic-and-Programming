61418be524bfb5f3acdad7a1025b7052f92fa872
Vova Pytsyuk <vovawed@gmail.com>
2017-09-28 23:09:46 +0300

    Додано 1 практичну, розділено на 2 папки по курсах

---
+++ 3cource/Practic1/cmake-build-debug/CMakeFiles/Practic1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Practic1.dir/main.cpp.o"
+  "Practic1.pdb"
+  "Practic1.exe"
+  "libPractic1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Practic1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 3cource/Practic1/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+class Person{
+private:
+    string name;
+    short dayOfBirth;
+    short monthOfBirth;
+public:
+    void input() {
+        cin.ignore();
+        cout << "Enter name: "; getline(cin, name);
+        cout << "Day of birthday: "; cin >> dayOfBirth;
+        cout << "Month of birthday: "; cin >> monthOfBirth;
+    }
+    void appendToFile(string filename) {
+        ofstream file(filename, fstream::app);
+        if (file){
+            file << name << setw(25 - name.length()) << dayOfBirth << '/' << monthOfBirth << endl;
+            file.close();
+        }
+        else {
+            exit(-1);
+        }
+    }
+};
+
+int main()
+{
+    Person p;
+    while (true) {
+        char check; cout << "Read(r), input(i) or quit(q): "; cin >> check;
+        string temp;
+        if (check == 'r') {
+            ifstream f("../database.txt");
+            if (f) {
+                cout << "Name" << setw(22) << "d/m" << endl << string(22, '-') << ' ' << string(4, '-') << endl;
+                while (!f.eof()) {
+                    getline(f, temp);
+                    cout << temp << endl;
+                }
+            }
+            else {
+                exit(-1);
+            }
+            f.close();
+        }
+        else if (check == 'i') {
+            p.input();
+            p.appendToFile("../database.txt");
+        }
+        else if (check == 'q') {
+            return 0;
+        }
+    }
+}
