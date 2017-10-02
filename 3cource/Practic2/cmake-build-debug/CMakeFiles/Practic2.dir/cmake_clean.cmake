cd1ef0a518c30fd22fd04003fd0f00cd6357f6fc
Vova Pytsyuk <vovawed@gmail.com>
2017-10-02 22:12:55 +0300

    Додано 2 практичну

---
+++ 3cource/Practic2/cmake-build-debug/CMakeFiles/Practic2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Practic2.dir/main.cpp.o"
+  "Practic2.pdb"
+  "Practic2.exe"
+  "libPractic2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Practic2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 3cource/Practic2/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void func(int** arr, int n)
+{
+    short line = n/6;
+    bool invert = 0;
+    for (int i = 0;  i < n; i++) {
+        if (i % line == 0) {
+            invert = !invert;
+        }
+        for (int x = 0; x < n; x++) {
+            if (invert) {
+                arr[i][x] = 0;
+            }
+        }
+    }
+}
+
+int main()
+{
+    const short size = 6; // Має ділитись на 6
+    int** array = new int*[size];
+    for(int i = 0; i < size; ++i) {
+        array[i] = new int[size];
+        fill_n(array[i], size, 1);  // Put 1 in each element
+    }
+
+    func(array, size);
+
+    for (int i = 0; i < size; i++) {
+        for (int x = 0; x < size; x++) {
+            cout << array[i][x];
+        }
+        cout << endl;
+    }
+}
